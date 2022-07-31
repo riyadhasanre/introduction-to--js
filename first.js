93be4e4f4da244b84237cc29d80b5edc80b371d2
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-07-31 11:17:24 +0600

    basic-javascript

---
+++ first.js
@@ -0,0 +1,26 @@
+
+
+var onion = 17;
+var egg = 5;
+
+var totalAddition = onion + egg;
+var totalSubtraction = onion - egg;
+var totalMultiplication = onion % egg;
+var totalDivision = onion / egg;
+console.log(totalAddition);
+console.log(totalSubtraction);
+console.log(totalMultiplication);
+var totalMultiplication = onion % egg;
+console.log(totalMultiplication);
+
+var harrysmomGave = 1000;
+var orangesPrice = 400;
+var applesPrice = 300;
+var totalPrice = orangesPrice + applesPrice;
+console.log(orangesPrice + applesPrice);
+var outputPrice = harrysmomGave - totalPrice;
+console.log(harrysmomGave - totalPrice);
+
+
+
+
