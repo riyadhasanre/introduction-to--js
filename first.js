d789cf7e5ab45f5ee7dd9f3fb2dab0186b5e6f59
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-07-31 12:02:35 +0600

    problem-solving

--- first.js
+++ first.js
@@ -12,6 +12,7 @@ console.log(totalSubtraction);
 console.log(totalMultiplication);
 var totalMultiplication = onion % egg;
 console.log(totalMultiplication);
+// ---problem solving 1--
 
 var harrysmomGave = 1000;
 var orangesPrice = 400;
@@ -20,7 +21,23 @@ var totalPrice = orangesPrice + applesPrice;
 console.log(orangesPrice + applesPrice);
 var outputPrice = harrysmomGave - totalPrice;
 console.log(harrysmomGave - totalPrice);
+// ---problem solving 2--
+var mathmatics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bagla = 99.50;
+var totlaSubjectmark = mathmatics + biology + chemistry + physics + bagla;
+console.log(totlaSubjectmark);
+var gorMark = totlaSubjectmark / 5;
+console.log(gorMark);
+// Problem solving 3
+var contains1 = "I am going to be"
+var contains2 = "an awesome webdeveloper"
+var totalContains = contains1 + " " + contains2;
+console.log(totalContains);
 
-
-
-
+// problem solving 4
+var saharsteacherGave = 119;
+var remainder = saharsteacherGave % 5;
+console.log(remainder);
