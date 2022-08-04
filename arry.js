ced20bc7c44a5fe4c9c5ceacc83147708460978c
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-08-04 11:46:57 +0600

    else

---
+++ arry.js
@@ -0,0 +1,9 @@
+var iphonePrice = 50000;
+var riyadDps = 20000;
+
+if (iphonePrice>= riyadDps) {
+    console.log("ami akta iphone kinbo na")
+}
+else {
+    console.log('ami kinbonode')
+}
---
+++ new.js
@@ -0,0 +1,28 @@
+// var naoyk = "miton";
+
+// if (naoyk=="anooto") {
+//     console.log("din tha day");
+// }
+// else if (naoyk == "miton") {
+//     console.log("marbo akhane");
+// }
+// else {
+//     console.log("amim jaqni anana");
+// }
+
+var fish = "roui";
+if (fish == "roui") {
+    console.log("dam thik ase");
+
+    if (fish=="pangas") {
+        console.log("dam kom kom ase");
+        
+    }
+} 
+
+else if (fish == "ilish") {
+    console.log("jatio mas");
+}
+else {
+    console.log("mass valo na");
+}
