6ce5066cdb1a2624866d7b638c4625150e8a3ef4
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-08-04 15:19:16 +0600

    function

--- new.js
+++ new.js
@@ -10,19 +10,51 @@
 //     console.log("amim jaqni anana");
 // }
 
-var fish = "roui";
-if (fish == "roui") {
-    console.log("dam thik ase");
+// var fish = "roui";
+// if (fish == "roui") {
+//     console.log("dam thik ase");
 
-    if (fish=="pangas") {
-        console.log("dam kom kom ase");
+//     if (fish=="pangas") {
+//         console.log("dam kom kom ase");
         
-    }
-} 
+//     }
+// }
 
-else if (fish == "ilish") {
-    console.log("jatio mas");
+// else if (fish == "ilish") {
+//     console.log("jatio mas");
+// }
+// else {
+//     console.log("mass valo na");
+// }
+
+// array------
+
+// var friends = ["mahim", "jibon", "akram", "karim"];
+// console.log(friends)
+// var friends = ["mahim", "jibon", "akram", "karim"];
+// friends.length = (2);
+// console.log(friends)
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.push("guava");
+// console.log(fruits);
+// fruits.unshift("painaple");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+
+// -----function-----
+// function bringSingara() {
+//     console.log("mama singara den");
+    
+// }
+// bringSingara();
+
+
+function bringSingara(money) {
+    console.log("mama singara den");
+    console.log(money)
 }
-else {
-    console.log("mass valo na");
-}
+bringSingara(300);
