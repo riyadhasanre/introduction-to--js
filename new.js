0e42b18b91b0bda27757a0580f7bbbf6aac6a7e6
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-08-04 15:21:58 +0600

    function-add

--- new.js
+++ new.js
@@ -44,17 +44,3 @@
 // console.log(fruits);
 // fruits.shift();
 // console.log(fruits);
-
-// -----function-----
-// function bringSingara() {
-//     console.log("mama singara den");
-    
-// }
-// bringSingara();
-
-
-function bringSingara(money) {
-    console.log("mama singara den");
-    console.log(money)
-}
-bringSingara(300);
---
+++ power.js
@@ -0,0 +1,14 @@
+
+// -----function-----
+// function bringSingara() {
+//     console.log("mama singara den");
+    
+// }
+// bringSingara();
+
+
+function bringSingara(money) {
+    console.log("mama singara den");
+    console.log(money)
+}
+bringSingara(300);
