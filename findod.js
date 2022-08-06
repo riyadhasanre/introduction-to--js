d337f02d2895dd0cc233c8f1a30906fe10d370e0
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-08-06 18:27:07 +0600

    function-const-let

---
+++ findod.js
@@ -0,0 +1,8 @@
+function getSum(numbers) {
+    for (let i = 0; i < numbers.length; i++){
+        const index = i;
+        console.log(index);
+    }
+}
+const myNumbers = [12, 25, 36, 75, 45, 78];
+getSum(myNumbers);
