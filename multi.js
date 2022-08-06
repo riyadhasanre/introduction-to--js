b76d305bb6491c56aba518ca9852937eb56ba472
unknown <riyadhasanre@gmail.comgit config --global user.name>
2022-08-07 02:22:15 +0600

    add

---
+++ multi.js
@@ -0,0 +1,10 @@
+function factorial(numbers) {
+    let result = 1;
+    for (let i = 1; i <= numbers; i++){
+        result = result * 1;
+    }
+        
+    return result;
+}
+const result = factorial(7);
+console.log(result);
---
+++ odnumbers.js
@@ -0,0 +1,7 @@
+function oddnumbers(thisnumber) {
+    for (let i = 0; i < thisnumber.lenght; i++){
+
+    }
+}
+const oddnumberskia = [15, 24, 36, 18, 45, 47, 42];
+console.log(oddnumberskia);
